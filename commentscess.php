a8c3c2d936d83bb44982555df4a7ba6de542828e
secondlnl <142692021+secondlnl@users.noreply.github.com>
2025-03-06 23:07:32 +0100

    Update commentscess.php

--- commentscess.php
+++ commentscess.php
@@ -1,19 +1,39 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
-    echo ("Sorry something went CRAZY but mostly wrong.");
+    // echo ("Sorry something went CRAZY but mostly wrong.");
 } else {
     include "config.php";
-    session_start();
-
-    //
-    $sql = "SELECT id, name, description, price FROM products";
-    $result = $conn->query($sql);
-    //
+    function show($conn)
+    {
+        $t = "";
+        $n = "";
+        $d = "";
+        $i = 0;
+        $pid = $_POST["PID"];
+        $showq = QueryMeThis("SELECT ID, Name, Text, Date FROM comments WHERE PID = ? ORDER BY ID DESC, Date DESC LIMIT 1;", ["i", $pid]);
+        if ($showq->num_rows > 0) {
+            // Output data of each row
+            while ($comment = $showq->fetch_assoc()) {
+                $res = array();
+                $i = $comment["ID"];
+                $n = $comment["Name"];
+                $t = $comment["Text"];
+                $d = $comment["Date"];
+                $p = array("id" => $i, "name" => $n, "text" => $t, "date" => $d);
+                $res[] = $p;
+            }
+            header('Content-Type: application/json');
+            echo json_encode($res);
+        }
+    }
     $prprd = mysqli_prepare($conn, "INSERT INTO comments (PID, Name,Text, Date) VALUES(?,?,?,?);");
     session_start();
-    mysqli_stmt_bind_param($prprd, "isss", $_POST["PID"], $_SESSION["un"], $_POST["comment"], (date("Y.m.d")));
+    $pid = $_POST["PID"];
+    $un = $_SESSION["un"];
+    $lcomment = $_POST["comment"];
+    $date = date("Y.m.d");
+    mysqli_stmt_bind_param($prprd, "isss", $pid, $un, $lcomment, $date);
     mysqli_stmt_execute($prprd);
     mysqli_stmt_close($prprd);
-    header("location: store.php");
-    mysqli_close($conn);
+    show($conn);
 }
